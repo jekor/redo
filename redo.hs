cc3b74d04cd845c2fd20089f5692fe502ad735f5
Chris Forno <jekor@jekor.com>
2013-04-05 20:07:04 +0000

    Fixed race condition, added redo target commandline option.

--- redo.hs
+++ redo.hs
@@ -1,5 +1,13 @@
-import System.Process
+import System.Environment (getArgs)
+import System.Process (createProcess, waitForProcess, shell)
 
+main :: IO ()
 main = do
-  _ <- createProcess $ shell "sh redo.do"
+  args <- getArgs
+  mapM_ redo args
+
+redo :: String -> IO ()
+redo target = do
+  (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do"
+  _ <- waitForProcess ph
   return ()
