caee2d2097bb274557698a71160a9d8ec13fc391
Chris Forno <jekor@jekor.com>
2013-05-28 23:28:21 +0000

    Pass along a REDO_TARGET environment variable to the .do script (and children).

--- redo.do
+++ redo.do
@@ -1,1 +1,2 @@
+redo-ifchange redo.hs
 ghc -v0 -o $3 redo.hs
--- redo.hs
+++ redo.hs
@@ -1,11 +1,23 @@
+{-# LANGUAGE StandaloneDeriving #-}
+
 import Control.Monad (filterM, liftM)
+import Data.Map.Lazy (insert, fromList, toList, adjust)
 import Data.Maybe (listToMaybe)
+import Debug.Trace (traceShow)
 import System.Directory (renameFile, removeFile, doesFileExist)
-import System.Environment (getArgs)
+import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
 import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
 import System.IO (hPutStrLn, stderr)
-import System.Process (createProcess, waitForProcess, shell)
+import System.Process (createProcess, waitForProcess, shell, CreateProcess(..), StdStream(..), CmdSpec(..))
+
+traceShow' arg = traceShow arg arg
+
+deriving instance Show CreateProcess
+
+deriving instance Show StdStream
+
+deriving instance Show CmdSpec
 
 main :: IO ()
 main = mapM_ redo =<< getArgs
@@ -14,15 +26,17 @@ redo :: String -> IO ()
 redo target = maybe printMissing redo' =<< redoPath target
  where redo' :: FilePath -> IO ()
        redo' path = do
-         (_, _, _, ph) <- createProcess $ shell $ cmd path
+         oldEnv <- getEnvironment
+         let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
+         (_, _, _, ph) <- createProcess $ traceShow' $ (shell $ cmd path) {env = Just newEnv}
          exit <- waitForProcess ph
-         case exit of
+         case traceShow' exit of
            ExitSuccess -> do renameFile tmp target
            ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
                                   removeFile tmp
        tmp = target ++ "---redoing"
        printMissing = error $ "No .do file found for target '" ++ target ++ "'"
-       cmd path = unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
+       cmd path = traceShow' $ unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
 
 redoPath :: FilePath -> IO (Maybe FilePath)
 redoPath target = listToMaybe `liftM` filterM doesFileExist candidates
