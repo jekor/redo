f8b53e5ac43069eebffb2b9ac542115342909454
Chris Forno <jekor@jekor.com>
2013-06-12 03:43:07 +0000

    Working (naive) implementation of redo-ifchange.

--- redo.do
+++ redo.do
@@ -1,2 +1,2 @@
-redo-ifchange redo.hs
+redo-ifchange redo.hs README.txt
 ghc -v0 -o $3 redo.hs
--- redo.hs
+++ redo.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import Control.Exception (catch, IOException)
-import Control.Monad (filterM, liftM, unless)
+import Control.Exception (catch, catchJust, IOException)
+import Control.Monad (filterM, liftM, unless, guard)
+import qualified Data.ByteString.Lazy as BL
+import Data.Digest.Pure.MD5 (md5)
 import Data.Map.Lazy (insert, fromList, toList, adjust)
 import Data.Maybe (listToMaybe)
 import Data.Typeable (typeOf)
 import Debug.Trace (traceShow)
 import GHC.IO.Exception (IOErrorType(..))
-import System.Directory (renameFile, removeFile, doesFileExist, getDirectoryContents)
+import System.Directory (renameFile, removeFile, doesFileExist, getDirectoryContents, removeDirectoryRecursive, createDirectoryIfMissing)
 import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
 import System.FilePath (hasExtension, replaceBaseName, takeBaseName, (</>))
-import System.IO (hPutStrLn, stderr)
-import System.IO.Error (ioeGetErrorType)
+import System.IO (hPutStrLn, stderr, hGetLine, withFile, IOMode(..))
+import System.IO.Error (ioeGetErrorType, isDoesNotExistError)
 import System.Process (createProcess, waitForProcess, shell, CreateProcess(..), StdStream(..), CmdSpec(..))
 
 traceShow' arg = traceShow arg arg
@@ -22,10 +24,15 @@ main = mapM_ redo =<< getArgs
 
 redo :: String -> IO ()
 redo target = do
-  upToDate' <- upToDate target
+  upToDate' <- upToDate target metaDepsDir
   unless upToDate' $ maybe printMissing redo' =<< redoPath target
  where redo' :: FilePath -> IO ()
        redo' path = do
+         catchJust (\e -> guard $ isDoesNotExistError e)
+                   (removeDirectoryRecursive metaDepsDir)
+                   (\_ -> return ())
+         createDirectoryIfMissing True metaDepsDir
+         writeFile (metaDepsDir </> path) =<< md5' path
          oldEnv <- getEnvironment
          let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
          (_, _, _, ph) <- createProcess $ (shell $ cmd path) {env = Just newEnv}
@@ -35,6 +42,7 @@ redo target = do
            ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
                                   removeFile tmp
        tmp = target ++ "---redoing"
+       metaDepsDir = ".redo" </> target
        printMissing = error $ "No .do file found for target '" ++ target ++ "'"
        cmd path = traceShow' $ unwords ["sh", path, "0", takeBaseName target, tmp, ">", tmp]
 
@@ -44,14 +52,17 @@ redoPath target = listToMaybe `liftM` filterM doesFileExist candidates
                                          then [replaceBaseName target "default" ++ ".do"]
                                          else []
 
-upToDate :: String -> IO Bool
-upToDate target = catch
-  (do deps <- getDirectoryContents depDir
+upToDate :: String -> FilePath -> IO Bool
+upToDate target metaDepsDir = catch
+  (do deps <- getDirectoryContents metaDepsDir
       (traceShow' . all id) `liftM` mapM depUpToDate deps)
   (\(e :: IOException) -> return False)
- where depDir = ".redo" </> target
-       depUpToDate :: FilePath -> IO Bool
+ where depUpToDate :: FilePath -> IO Bool
        depUpToDate dep = catch
-         (do oldMD5 <- traceShow' `liftM` readFile (depDir </> dep)
-             return False)
+         (do oldMD5 <- withFile (metaDepsDir </> dep) ReadMode hGetLine
+             newMD5 <- md5' dep
+             return $ oldMD5 == newMD5)
          (\e -> return (ioeGetErrorType e == InappropriateType))
+
+md5' :: FilePath -> IO String
+md5' path = (show . md5) `liftM` BL.readFile path
