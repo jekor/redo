e4bf1eb0fed5e9e5bf085f6cbd600d0a8898ba2f
Chris Forno <jekor@jekor.com>
2013-07-30 21:55:45 +0000

    Switching from install.do script to cabal install for (hopefully) better cross-platform compatibility.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,18 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.InstallDirs (InstallDirs(..), fromPathTemplate, toPathTemplate)
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(..))
+import Distribution.Simple.Setup (ConfigFlags(..), fromFlag, fromFlagOrDefault)
+import System.FilePath ((</>))
+import System.Posix.Files (createSymbolicLink)
+
+main = defaultMainWithHooks simpleUserHooks {postInst = postInstall}
+
+postInstall _ _ _ buildInfo = do
+  -- I tried the following seemingly more correct way but received the error:
+  -- "internal error InstallDirs.libsubdir". For now I gave up on tracking it
+  -- down and switched to the following code. If you know what I'm doing wrong
+  -- here, please let me know.
+  -- let installDirs = absoluteInstallDirs pkgDesc buildInfo NoCopyDest
+  let dirs = configInstallDirs $ configFlags buildInfo
+      dir = (fromPathTemplate . fromFlag $ prefix dirs) </> (fromPathTemplate . fromFlagOrDefault (toPathTemplate "bin") $ bindir dirs)
+  createSymbolicLink (dir </> "redo") (dir </> "redo-ifchange")
--- install.do
+++
@@ -1,3 +0,0 @@
-redo-ifchange redo
-sudo install -s redo /usr/local/bin
-sudo ln -sf /usr/local/bin/redo /usr/local/bin/redo-ifchange
